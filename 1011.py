cb62dd9de3f29c57c7fc44693e8c54104a293357
songjahao <994628768@qq.com>
2025-01-01 21:22:19 +0800

    add 1011

--- 1011.py
+++ 1011.py
@@ -1,7 +1,13 @@
 # 1011 A+B和C
 
 def solution():
-    pass
+    n = int(input())
+    for i in range(n):
+        a, b, c = map(int, input().split())
+        if a + b > c:
+            print("Case #%d: true" % (i + 1))
+        else:
+            print("Case #%d: false" % (i + 1))
 
 if __name__ == "__main__":
     solution()
